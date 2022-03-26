df3fe538422bfb71af06f01946f590702b268e98
Billy  G <me@billyg.sh>
2022-03-26 15:42:30 -0400

    removed beta kernel

--- update.sh
+++ update.sh
@@ -18,14 +18,15 @@
 #}
 
 install_updatedkernel() {
-    cd ~/rhinoupdate/kernel/
-    wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-headers-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
-    wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-headers-5.17.0-051700_5.17.0-051700.202203202130_all.deb
-    wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-image-unsigned-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
-    wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-modules-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
-    dpkg -i  *.deb
-    sudo apt --fix-broken install
+#    cd ~/rhinoupdate/kernel/
+#    wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-headers-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
+#    wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-headers-5.17.0-051700_5.17.0-051700.202203202130_all.deb
+#    wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-image-unsigned-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
+#    wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-modules-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
+#    dpkg -i  *.deb
+#    sudo apt --fix-broken install
     sudo apt update
     sudo apt upgrade
+    sudo apt dist-upgrade
     print "You will need to reboot after the script finishes."
 }
