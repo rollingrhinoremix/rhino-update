feb005cd3780f60d6443057e295f689c02f9a00f
Billy Gilbrech <Billy Gilbrech>
2022-04-04 11:25:51 -0400

    added MrBeeBenson detection code, which will be updated soon in the future.

--- update.sh
+++ update.sh
@@ -11,22 +11,11 @@
 #    sed -E 's/.*"([^"]+)".*/\1/'
 #}
 
-# Update system packages
-#install_newpackages() {
-#    sudo apt update
-#    sudo apt upgrade
-#}
-
-# Download the latest kernel
-# cd ~/rhinoupdate/kernel/
-# wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-headers-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
-# wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-headers-5.17.0-051700_5.17.0-051700.202203202130_all.deb
-# wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-image-unsigned-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
-# wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-modules-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
-
-# Install the latest kernel onto the system
-# dpkg -i  *.deb
-# sudo apt --fix-broken install
+#Check if Rhino update is configured. 
+if [ ! -f "$HOME/.rhino/updates/configuration" ]; then
+ # Code will go here 
+  touch "$HOME/.rhino/updates/configuration"
+fi
 
 # Perform system upgrade
 sudo apt update
