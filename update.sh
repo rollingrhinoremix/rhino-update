92bef170630a7bfeb162d3636e9afa1bec1278d5
MrBeeBenson <90976178+MrBeeBenson@users.noreply.github.com>
2022-04-04 17:58:11 +0100

    Add files via upload

--- update.sh
+++ update.sh
@@ -5,9 +5,30 @@
 # URLs
 # https://rollingrhinoremix.github.io/
 
-# Perform system upgrade
+# Check to see whether the "configuration update", released in 2022.04.14 has been applied.
+if [ ! -f "$HOME/.rhino/updates/configuration" ]; then
+  mkdir ~/.rhino
+  mkdir ~/.rhino/config
+  mkdir ~/.rhino/updates
+  echo "alias rhino-config='mkdir ~/.rhino/config/config-script && git clone https://github.com/rollingrhinoremix/rhino-config ~/.rhino/config/config-script/ && python3 ~/.rhino/config/config-script/config.py && rm -rf ~/.rhino/config/config-script'" >> .bashrc
+  touch "$HOME/.rhino/updates/configuration"
+fi
+
+# If the user has selected the option to install the mainline kernel, install it onto the system.
+if [ -f "$HOME/.rhino/config/mainline" ]; then
+  cd ~/rhinoupdate/kernel/
+  wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-headers-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
+  wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-headers-5.17.0-051700_5.17.0-051700.202203202130_all.deb
+  wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-image-unsigned-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
+  wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17/amd64/linux-modules-5.17.0-051700-generic_5.17.0-051700.202203202130_amd64.deb
+  dpkg -i  *.deb
+  sudo apt --fix-broken install
+fi
+
+# Perform full system upgrade
 sudo apt update
 sudo apt dist-upgrade
+
 # Allow the user to know that the upgrade has completed
 echo "---"
 echo "You will need to reboot after the script finishes."
