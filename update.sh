73832c29501955dbc6e9c4aa7667623eeefdaa0a
MrBeeBenson <90976178+MrBeeBenson@users.noreply.github.com>
2022-04-04 17:42:38 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,18 +5,6 @@
 # URLs
 # https://rollingrhinoremix.github.io/
 
-#get_latest_release() {
-#   curl --silent "https://api.github.com/repos/$1/releases/latest" |
-#    grep '"tag_name":' |
-#    sed -E 's/.*"([^"]+)".*/\1/'
-#}
-
-#Check if Rhino update is configured. 
-#if [ ! -f "$HOME/.rhino/updates/configuration" ]; then
- # Code will go here 
-#  touch "$HOME/.rhino/updates/configuration"
-#fi
-
 # Perform system upgrade
 sudo apt update
 sudo apt dist-upgrade
