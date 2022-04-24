1c8c5c20224cd54f83a943c5e31fe9b2f16ff2ec
James Billy Ray Gilbrech <me@billyg.sh>
2022-04-24 09:18:13 -0400

    updated kernel

--- update.sh
+++ update.sh
@@ -5,48 +5,45 @@
 # URLs
 # https://rollingrhinoremix.github.io/
 
+set -e
+
 # Check to see whether the "configuration update", released in 2022.04.14 has been applied.
-if [ ! -f "$HOME/.rhino/updates/configuration" ]; then
-  mkdir ~/.rhino
-  mkdir ~/.rhino/config
-  mkdir ~/.rhino/updates
+if [[ ! -f "$HOME/.rhino/updates/configuration" ]]; then
+  mkdir -p ~/.rhino/{config,updates}
   echo "alias rhino-config='mkdir ~/.rhino/config/config-script && git clone https://github.com/rollingrhinoremix/rhino-config ~/.rhino/config/config-script/ && python3 ~/.rhino/config/config-script/config.py && rm -rf ~/.rhino/config/config-script'" >> ~/.bashrc
   touch "$HOME/.rhino/updates/configuration"
 fi
 
-# Remove SnapD
-sudo rm -rf /var/cache/snapd/
-sudo apt autoremove --purge snapd gnome-software-plugin-snap
-sudo rm -fr ~/snap
-sudo apt-mark hold snapd
-
-# Install Flatpak
-sudo apt install flatpak
-sudo apt install gnome-software-plugin-flatpak
-
-# Configure FlatHub as the default repoistory for Flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# Install FlatSeal for configuring permissions on FlatHub apps
-flatpak install flathub com.github.tchx84.Flatseal
-
 # If the user has selected the option to install the mainline kernel, install it onto the system.
-if [ -f "$HOME/.rhino/config/mainline" ]; then
+if [[ -f "$HOME/.rhino/config/mainline" ]]; then
   cd ~/rhinoupdate/kernel/
-  wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.1/amd64/linux-headers-5.17.1-051701-generic_5.17.1-051701.202203280950_amd64.deb
-  wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.1/amd64/linux-headers-5.17.1-051701_5.17.1-051701.202203280950_all.deb
-  wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.1/amd64/linux-image-unsigned-5.17.1-051701-generic_5.17.1-051701.202203280950_amd64.deb
-  wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.1/amd64/linux-modules-5.17.1-051701-generic_5.17.1-051701.202203280950_amd64.deb
-  sudo dpkg -i  *.deb
-  sudo apt --fix-broken install
+  wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.4/amd64/linux-headers-5.17.4-051704-generic_5.17.4-051704.202204200842_amd64.deb
+  wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.4/amd64/linux-headers-5.17.4-051704_5.17.4-051704.202204200842_all.deb
+  wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.4/amd64/linux-image-unsigned-5.17.4-051704-generic_5.17.4-051704.202204200842_amd64.deb
+  wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.4/amd64/linux-modules-5.17.4-051704-generic_5.17.4-051704.202204200842_amd64.deb
+  sudo apt install ./*.deb
 fi
 
-# Perform full system upgrade
+# If snapd is installed.
+if [[ ! -f "$HOME/.rhino/config/snapdpurge" ]]; then
+  sudo snap refresh
+fi
+
+
+# If Pacstall has been enabled
+if [[ -f "$HOME/.rhino/config/pacstall" ]]; then
+  mkdir -p ~/rhinoupdate/pacstall/
+  cd ~/rhinoupdate/pacstall/
+  wget -q --show-progress --progress=bar:force https://github.com/pacstall/pacstall/releases/download/1.7.3/pacstall-1.7.3.deb
+  sudo apt install ./*.deb
+  pacstall -Up
+fi
+
+# Perform full system upgrade.
 sudo apt update
 sudo apt dist-upgrade
 
-# Allow the user to know that the upgrade has completed
-echo "---"
-echo "System will be rebooted in 1 minute."
-echo "---"0
-sudo reboot -h +1
+# Allow the user to know that the upgrade has completed.
+echo "---
+The upgrade has been completed. Please reboot your system to see the changes.
+---"
