5023272bfc05b3c9b3b7ce1af8cb5681beedc62d
MrBeeBenson <90976178+MrBeeBenson@users.noreply.github.com>
2022-04-25 12:51:47 +0100

    fix: Fix additional indent
    Fix the additional indent inside of an if statement, which would've provided a syntax error.

--- update.sh
+++ update.sh
@@ -32,7 +32,7 @@ sudo mv rhino-config /usr/bin
 # If the user has selected the option to install the mainline kernel, install it onto the system.
 if [[ -f "$HOME/.rhino/config/mainline" ]]; then
   cd ~/rhinoupdate/kernel/
-    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.4/amd64/linux-headers-5.17.4-051704-generic_5.17.4-051704.202204200842_amd64.deb
+  wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.4/amd64/linux-headers-5.17.4-051704-generic_5.17.4-051704.202204200842_amd64.deb
   wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.4/amd64/linux-headers-5.17.4-051704_5.17.4-051704.202204200842_all.deb
   wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.4/amd64/linux-image-unsigned-5.17.4-051704-generic_5.17.4-051704.202204200842_amd64.deb
   wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.4/amd64/linux-modules-5.17.4-051704-generic_5.17.4-051704.202204200842_amd64.deb
