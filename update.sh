a0bc121b0c3824573dcb0423d91830c5dd75afe9
MrBeeBenson <airriflepellet@gmail.com>
2022-05-04 14:29:52 +0100

    fix(update.sh): Pacstall dependancies are not automatically getting resolved.

--- update.sh
+++ update.sh
@@ -38,7 +38,7 @@ if [[ -f "$HOME/.rhino/config/mainline" ]]; then
   wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.5/amd64/linux-image-unsigned-5.17.5-051705-generic_5.17.5-051705.202204271406_amd64.deb
   wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.5/amd64/linux-modules-5.17.5-051705-generic_5.17.5-051705.202204271406_amd64.deb
   sudo dpkg -i *.deb
-  sudo apt --fix-broken install
+  sudo apt --fix-broken install -y
 fi
 
 # If snapd is installed.
@@ -53,7 +53,7 @@ if [[ -f "$HOME/.rhino/config/pacstall" ]]; then
   cd ~/rhinoupdate/pacstall/
   wget -q --show-progress --progress=bar:force https://github.com/pacstall/pacstall/releases/download/1.7.3/pacstall-1.7.3.deb
   sudo dpkg -i *.deb
-  sudo apt --fix-broken install
+  sudo apt --fix-broken install -y
   pacstall -Up
 fi
 
