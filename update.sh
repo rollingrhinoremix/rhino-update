cd3cd16c8e8e1c78a495da3271fc754953716e45
MrBeeBenson <airriflepellet@gmail.com>
2022-05-04 15:41:42 +0100

    feat(update.sh): Add new section to ensure that system files are kept up-to-date
    Add new section to ensure that system files are kept up-to-date. Comment out the installation of Pacstall due to Ubuntu dependency errors.

--- update.sh
+++ update.sh
@@ -37,8 +37,7 @@ if [[ -f "$HOME/.rhino/config/mainline" ]]; then
   wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.5/amd64/linux-headers-5.17.5-051705_5.17.5-051705.202204271406_all.deb
   wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.5/amd64/linux-image-unsigned-5.17.5-051705-generic_5.17.5-051705.202204271406_amd64.deb
   wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.17.5/amd64/linux-modules-5.17.5-051705-generic_5.17.5-051705.202204271406_amd64.deb
-  sudo dpkg -i *.deb
-  sudo apt --fix-broken install -y
+  sudo apt install ./*.deb
 fi
 
 # If snapd is installed.
@@ -49,17 +48,23 @@ fi
 
 # If Pacstall has been enabled
 if [[ -f "$HOME/.rhino/config/pacstall" ]]; then
-  #sudo bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
-  sudo apt install curl -y
-  mkdir -p ~/rhinoupdate/pacstall/
-  cd ~/rhinoupdate/pacstall/
-  wget -q --show-progress --progress=bar:force https://github.com/pacstall/pacstall/releases/download/1.7.3/pacstall-1.7.3.deb
-  sudo apt --fix-broken install ./pacstall-1.7.3.deb
-  pacstall -Up
+  #mkdir -p ~/rhinoupdate/pacstall/
+  #cd ~/rhinoupdate/pacstall/
+  #wget -q --show-progress --progress=bar:force https://github.com/pacstall/pacstall/releases/download/1.7.3/pacstall-1.7.3.deb
+  #sudo apt install ./*.deb
+  #pacstall -Up
 fi
 
 # Perform full system upgrade.
 { sudo apt update 2> /dev/null; sudo apt dist-upgrade 2> /dev/null; }
+echo "Pacstall has been disabled due to an issue with Ubuntu dependencies, and will be re-enabled at a later date."
+
+# Ensure system files are kept up-to-date
+mkdir ~/rhinoupdate/system-files-update
+git clone https://github.com/rollingrhinoremix/distro ~/rhinoupdate/system-files-update
+cd ~/rhinoupdate/distro-update
+mv .bash_aliases ~
+sudo mv os-release /etc
 
 # Allow the user to know that the upgrade has completed.
 echo "---
