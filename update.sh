93dea39715e69939b58006155741cdb8e367b52f
cat-master21 <96554164+cat-master21@users.noreply.github.com>
2022-05-27 09:33:16 -0700

    feat(update.sh): use FHS directories

--- update.sh
+++ update.sh
@@ -8,30 +8,30 @@
 set -e
 
 # Check to see whether the "configuration update", released in 2022.04.19 has been applied.
-if [[ ! -f "$HOME/.rhino/updates/configuration" ]]; then
-  mkdir -p ~/.rhino/{config,updates}
-  echo "alias rhino-config='mkdir ~/.rhino/config/config-script && git clone https://github.com/rollingrhinoremix/rhino-config ~/.rhino/config/config-script/ && python3 ~/.rhino/config/config-script/config.py && rm -rf ~/.rhino/config/config-script'" >> ~/.bashrc
-  : > "$HOME/.rhino/updates/configuration"
+if [[ ! -f "/usr/share/rhino/updates/configuration" ]]; then
+  mkdir -p /usr/share/rhino/{config,updates}
+  echo "alias rhino-config='mkdir /usr/share/rhino/config/config-script && git clone https://github.com/rollingrhinoremix/rhino-config /usr/share/rhino/config/config-script/ && python3 /usr/share/rhino/config/config-script/config.py && rm -rf /usr/share/rhino/config/config-script'" >> ~/.bashrc
+  : > "/usr/share/rhino/updates/configuration"
 fi
 
 # Check to see whether the rhino-config v2 update has been applied, which converts Rhino into a command-line utility.
-if [[ ! -f "$HOME/.rhino/updates/config-v2" ]]; then
-  mkdir ~/rhinoupdate/distro
-  git clone https://github.com/rollingrhinoremix/distro ~/rhinoupdate/distro
-  mv ~/rhinoupdate/distro/.{bashrc,bash_aliases} ~
-  : > "$HOME/.rhino/updates/config-v2"
+if [[ ! -f "/usr/share/rhino/updates/config-v2" ]]; then
+  mkdir /usr/share/rhino/rhinoupdate/distro
+  git clone https://github.com/rollingrhinoremix/distro /usr/share/rhino/rhinoupdate/distro
+  mv /usr/share/rhino/rhinoupdate/distro/.{bashrc,bash_aliases} ~
+  : > "/usr/share/rhino/updates/config-v2"
 fi
 
 # Install latest rhino-config utility
-mkdir ~/rhino-config
-cd ~/rhino-config
+mkdir /usr/share/rhino/rhino-config
+cd /usr/share/rhino/rhino-config
 wget -q --show-progress --progress=bar:force https://github.com/rollingrhinoremix/rhino-config/releases/download/v2.0.1/rhino-config
 chmod +x rhino-config
 sudo mv rhino-config /usr/bin
-rm -rf ~/rhino-config
+rm -rf /usr/share/rhino/rhino-config
 
 # If the user has selected the option to install the mainline kernel, install it onto the system.
-if [[ -f "$HOME/.rhino/config/mainline" ]] && [[ ! -f "$HOME/.rhino/config/5-18-0" ]]; then
+if [[ -f "/usr/share/rhino/config/mainline" ]] && [[ ! -f "/usr/share/rhino/config/5-18-0" ]]; then
     cd ~/rhinoupdate/kernel/
     wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18/amd64/CHECKSUMS
     wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18/amd64/linux-headers-5.18.0-051800-generic_5.18.0-051800.202205222030_amd64.deb
@@ -42,7 +42,7 @@ if [[ -f "$HOME/.rhino/config/mainline" ]] && [[ ! -f "$HOME/.rhino/config/5-18-
     echo "Verifying checksums..."
     if shasum --check --ignore-missing CHECKSUMS; then
       sudo apt install ./*.deb
-      : > "$HOME/.rhino/config/5-18-0"
+      : > "/usr/share/rhino/config/5-18-0"
     else
       >&2 echo "Failed to verify checksums of downloaded kernel files!"
       exit 1
@@ -50,23 +50,23 @@ if [[ -f "$HOME/.rhino/config/mainline" ]] && [[ ! -f "$HOME/.rhino/config/5-18-
 fi
 
 # If snapd is installed.
-if [[ ! -f "$HOME/.rhino/config/snapdpurge" ]]; then
+if [[ ! -f "/usr/share/rhino/config/snapdpurge" ]]; then
   sudo snap refresh
 fi
 
 
 # If Pacstall has been enabled
-if [[ -f "$HOME/.rhino/config/pacstall" ]]; then
+if [[ -f "/usr/share/rhino/config/pacstall" ]]; then
 # Check to see whether an issue in Curl has been fixed
-  if [[ ! -f "$HOME/.rhino/config/curl-fix" ]]; then
+  if [[ ! -f "/usr/share/rhino/config/curl-fix" ]]; then
     sudo apt remove libcurl4 -y
     sudo apt autoremove -y 
     sudo apt install libcurl4 curl -y
-    : > "$HOME/.rhino/config/curl-fix"
+    : > "/usr/share/rhino/config/curl-fix"
   fi
   # Install Pacstall
-  mkdir -p ~/rhinoupdate/pacstall/
-  cd ~/rhinoupdate/pacstall/
+  mkdir -p /usr/share/rhino/rhinoupdate/pacstall/
+  cd /usr/share/rhino/rhinoupdate/pacstall/
   wget -q --show-progress --progress=bar:force https://github.com/pacstall/pacstall/releases/download/1.7.3/pacstall-1.7.3.deb
   sudo apt install ./*.deb
   pacstall -Up
