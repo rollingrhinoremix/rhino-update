98b4ab5cd1a5bd7a83473d7133241a92f86b79ca
Chadano <airriflepellet@gmail.com>
2022-06-29 23:56:53 +0100

    update(update.sh): Update kernel to 5.18.8

--- update.sh
+++ update.sh
@@ -44,19 +44,19 @@ chmod +x rhino-deinst
 sudo mv rhino-deinst /usr/bin
 
 # Automatically install the latest Linux kernel onto the system if it has not been installed already.
-if [[ ! -f "$HOME/.rhino/config/5-18-6" ]]; then
+if [[ ! -f "$HOME/.rhino/config/5-18-8" ]]; then
     cd ~/rhinoupdate/kernel/
-    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.6/amd64/CHECKSUMS &
-    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.6/amd64/linux-headers-5.18.6-051806-generic_5.18.6-051806.202206221336_amd64.deb &
-    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.6/amd64/linux-headers-5.18.6-051806_5.18.6-051806.202206221336_all.deb &
-    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.6/amd64/linux-image-unsigned-5.18.6-051806-generic_5.18.6-051806.202206221336_amd64.deb &
-    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.6/amd64/linux-modules-5.18.6-051806-generic_5.18.6-051806.202206221336_amd64.deb &
+    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.8/amd64/CHECKSUMS &
+    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.8/amd64/linux-headers-5.18.8-051808-generic_5.18.8-051808.202206290850_amd64.deb &
+    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.8/amd64/linux-headers-5.18.8-051808_5.18.8-051808.202206290850_all.deb &
+    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.8/amd64/linux-image-unsigned-5.18.8-051808-generic_5.18.8-051808.202206290850_amd64.deb &
+    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.8/amd64/linux-modules-5.18.8-051808-generic_5.18.8-051808.202206290850_amd64.deb &
     wait
     
     echo "Verifying checksums..."
     if shasum --check --ignore-missing CHECKSUMS; then
       sudo apt install ./*.deb
-      : > "$HOME/.rhino/config/5-18-6"
+      : > "$HOME/.rhino/config/5-18-8"
     else
       >&2 echo "Failed to verify checksums of downloaded kernel files!"
       exit 1
