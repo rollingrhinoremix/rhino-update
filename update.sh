c0bc91c40be6aa99322d7c3d66f0b535a31a55ff
Sourajyoti Basak <wiz28@protonmail.com>
2022-07-09 22:04:58 +0530

    feat: add ability to install xanmod variants

--- update.sh
+++ update.sh
@@ -64,11 +64,31 @@ if [[ ! -f "$HOME/.rhino/config/5-18-8" ]]; then
     fi
 fi
 
-# If the user has enabled the xanmod kernel via rhino-config, install it.
-if [[ -f "$HOME/.rhino/config/xanmod" ]]; then
+# If the user has enabled a xanmod kernel variant via rhino-config, install it.
+xanmod_variants=$(compgen -G "$HOME/.rhino/config/xanmod-*")
+if $xanmod_variants; then
     echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
     wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
-    sudo apt update && sudo apt install linux-xanmod
+    sudo apt update
+    
+    for variant in $xanmod_variants; do
+    	case $variant in
+    		stable)
+    			sudo apt install linux-xanmod
+    		;;
+    		realtime)
+    			sudo apt install linux-xanmod-rt
+    		;;
+    		realtime_edge)
+    			sudo apt install linux-xanmod-rt-edge
+    		;;
+    		tasktype)
+    			sudo apt install linux-xanmod-tt
+    		;;
+    		*)
+    			sudo apt install linux-$variant
+    		;;
+    	esac
 fi
 
 # If the user has enabled the liq kernel via rhino-config, install it.
