dc5ecf700c606a4be9da499f019c08caf8af6c52
Emerson Castillo <61599413+emberavenge@users.noreply.github.com>
2022-07-31 15:38:06 -0400

    Updated linux kernel links and file version number

--- update.sh
+++ update.sh
@@ -45,19 +45,19 @@ chmod +x rhino-deinst
 sudo mv rhino-deinst /usr/bin
 
 # Automatically install the latest Linux kernel onto the system if it has not been installed already.
-if [[ ! -f "$HOME/.rhino/config/5-18-14" ]]; then
+if [[ ! -f "$HOME/.rhino/config/5-18-15" ]]; then
     cd ~/rhinoupdate/kernel/
-    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.14/amd64/CHECKSUMS &
-    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.14/amd64/linux-headers-5.18.14-051814-generic_5.18.14-051814.202207231131_amd64.deb &
-    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.14/amd64/linux-headers-5.18.14-051814_5.18.14-051814.202207231131_all.deb &
-    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.14/amd64/linux-image-unsigned-5.18.14-051814-generic_5.18.14-051814.202207231131_amd64.deb &
-    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.14/amd64/linux-modules-5.18.14-051814-generic_5.18.14-051814.202207231131_amd64.deb &
+    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/CHECKSUMS
+    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-headers-5.18.15-051815-generic_5.18.15-051815.202207291649_amd64.deb &
+    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-headers-5.18.15-051815_5.18.15-051815.202207291649_all.deb &
+    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-image-unsigned-5.18.15-051815-generic_5.18.15-051815.202207291649_amd64.deb &
+    wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.18.15/amd64/linux-modules-5.18.15-051815-generic_5.18.15-051815.202207291649_amd64.deb &
     wait
     
     echo "Verifying checksums..."
     if shasum --check --ignore-missing CHECKSUMS; then
       sudo apt install ./*.deb
-      : > "$HOME/.rhino/config/5-18-14"
+      : > "$HOME/.rhino/config/5-18-15"
     else
       >&2 echo "Failed to verify checksums of downloaded kernel files!"
       exit 1
