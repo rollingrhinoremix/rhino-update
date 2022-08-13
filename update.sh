80411b490544fb1ec15eeeb0e99543278f505f16
X4X5 <50627538+X4X5@users.noreply.github.com>
2022-08-13 17:28:53 -0400

    Update update.sh
    Should fix the missing lines

--- update.sh
+++ update.sh
@@ -45,7 +45,7 @@ chmod +x rhino-deinst
 sudo mv rhino-deinst /usr/bin
 
 # Automatically install the latest Linux kernel onto the system if it has not been installed already. Also ensures that the system is running a pure Linux installation and not RRR installed within WSL.
-if [[ ! -f "$HOME/.rhino/config/5-19-0" ]] && [[ ! -f "$HOME/.rhino/config/wsl-yes" ]]; then
+if [[ ! -f "$HOME/.rhino/config/5-19-1" ]] && [[ ! -f "$HOME/.rhino/config/wsl-yes" ]]; then
     cd ~/rhinoupdate/kernel/
     wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.19.1/amd64/CHECKSUMS &
     wget -q --show-progress --progress=bar:force https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.19.1/amd64/linux-headers-5.19.1-051901-generic_5.19.1-051901.202208111238_amd64.deb &
@@ -57,7 +57,7 @@ if [[ ! -f "$HOME/.rhino/config/5-19-0" ]] && [[ ! -f "$HOME/.rhino/config/wsl-y
     echo "Verifying checksums..."
     if shasum --check --ignore-missing CHECKSUMS; then
       sudo apt install ./*.deb
-      : > "$HOME/.rhino/config/5-19-0"
+      : > "$HOME/.rhino/config/5-19-1"
     else
       >&2 echo "Failed to verify checksums of downloaded kernel files!"
       exit 1
